14df1983ed176498e69be4b26abef1344050dc67
Vinicius <vsxavier@live.com>
2019-07-07 22:16:59 -0300

    Lista 3 concluida

---
+++ Knapsack1_Gerador.py
@@ -0,0 +1,77 @@
+from random import randint
+from copy import copy
+
+n = 20
+r= 5
+W=[]
+P=[]
+M=0
+
+OptP = 0
+OptX = []
+
+def Knapsack1(l, X):
+    global OptP, OptX,W,P,M
+    if l==n:
+        currentWeigth = 0
+        for i in range(n):
+            currentWeigth += X[i]*W[i]
+        
+        if currentWeigth <= M:
+            CurP = 0
+            for i in range(n):
+                CurP += P[i]*X[i]
+            if CurP > OptP:
+                OptP = CurP
+                OptX = copy(X)
+    else:
+        X[l] = 1
+        Knapsack1(l+1, X)
+        X[l] = 0
+        Knapsack1(l+1, X)
+        
+        
+def knapsack_uncorrelated(n,r):
+    pesos, valores = [], []
+    for i in range(n):
+        pesos.append(randint(1,r))
+        valores.append(randint(1,r))
+    capacidade = sum(pesos)/1000
+    capacidade = r + 1 if capacidade <= r else capacidade
+    return pesos, valores, capacidade
+
+def knapsack_weakly_correlated(n,r):
+    pesos, valores = [],[]   
+    for i in range(n):
+        pesos.append(randint(1,r))
+        valores.append(max(pesos[i]+randint(1,r/5 +1)-r,1))             
+    capacidade = sum(pesos)/1000
+    capacidade =  r + 1 if capacidade <= r else capacidade
+    return pesos, valores, capacidade
+
+def knapsack_strongly_correlated(n,r):
+    pesos, valores = [], []
+    for i in range(n):
+        pesos.append(randint(1,r))
+        valores.append(pesos[i]+10)
+    capacidade = sum(pesos)/1000
+    capacidade = r + 1 if capacidade <= r else capacidade
+    return pesos, valores, capacidade        
+
+   
+   
+W,P,M = knapsack_strongly_correlated(n, r)
+
+def generateX():
+    X=[]
+    for i in range(n):
+        X.append(0)
+    return X
+
+X=generateX()
+Knapsack1(0, X)
+print("W: ", W)
+print("P: ", P)
+print("M: ", M)
+print("OptP: ", OptP)
+print("OptX: ", OptX)
---
+++ Knapsack2_TutoOR.py
@@ -0,0 +1,51 @@
+from random import randint
+from copy import copy
+from urllib.request import urlopen
+
+numero_instancia = 8 # pode ser de 1 a 8
+
+url_base = "http://people.sc.fsu.edu/~jburkardt%20/datasets/knapsack_01"
+with urlopen("{}/p0{}_w.txt".format(url_base, numero_instancia)) as arquivo:
+    pesos = [int(linha) for linha in arquivo.readlines()]
+with urlopen("{}/p0{}_p.txt".format(url_base, numero_instancia)) as arquivo:
+    valores = [int(linha) for linha in arquivo.readlines()]
+with urlopen("{}/p0{}_c.txt".format(url_base, numero_instancia)) as arquivo:
+    capacidade = int(arquivo.read())
+n = len(pesos)
+
+
+OptP = 0
+OptX = []
+
+       
+def Knapsack2(l, CurW, X):
+    global OptP, OptX,pesos,valores,capacidade
+    if l==n:
+        currentProfit = 0
+        for i in range(n):
+            currentProfit += X[i]*valores[i]
+        if currentProfit>OptP:
+            OptP = currentProfit
+            OptX = copy(X)
+    else:
+        if CurW + pesos[l]<=capacidade:
+            X[l] = 1
+            Knapsack2(l+1, CurW+pesos[l], X)
+        X[l] = 0
+        Knapsack2(l+1, CurW, X)
+        
+
+def generateX():
+    X=[]
+    for i in range(n):
+        X.append(0)
+    return X
+
+X=generateX()
+Knapsack2(0, 0, X)
+
+print("pesos: ", pesos)
+print("valores: ", valores)
+print("Capacidade: ", capacidade)
+print("OptP: ", OptP)
+print("OptX: ", OptX)
